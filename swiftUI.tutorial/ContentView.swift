5470e0614be5581488ff3d7835ef550d6689f317
Ciri Ryan <cirijaye@yahoo.com>
2022-04-07 14:53:12 -0700

    Initial Commit

---
+++ swiftUI.tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI.tutorial
+//
+//  Created by Ciri Ryan on 4/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI.tutorial/swiftUI_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_tutorialApp.swift
+//  swiftUI.tutorial
+//
+//  Created by Ciri Ryan on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
